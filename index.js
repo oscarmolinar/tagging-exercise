03158c38702a4aa342ef3268a246fd26d20bc5a5
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2023-10-10 13:07:01 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//Evento para gestionar el formulario de login
+document.getElementById("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const email = document.querySelector("input[type='email']").value;
+    const password = document.querySelector("input[type='password']").value;
+
+    console.log(email, password)
+})
+
+//Evento para gestionar los productos seleccionados
+document.querySelectorAll("#product-container .btn").forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        e.preventDefault()
+        const product_name = e.target.previousElementSibling.previousElementSibling.innerHTML
+        console.log(product_name)
+    })
+
+})
